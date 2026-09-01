8f546a0701b2dea9ae85bdb2b51329e02bf8cb99
chaitos <advsanture@gmail.com>
2026-09-01 20:28:01 +0600

    fixed some bugs add readme file

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os.path
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n#i58)s&2_#0cy0zslyp9kuga0787+9fi2_kr@b6@%1$3dlrr0'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
